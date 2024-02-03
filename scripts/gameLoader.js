df8fdfc3eabc2e9a9fd7b581a1eba4b7989b4389
albertsparrman <albert.sparrman@gmail.com>
2024-02-03 22:05:11 +0100

    sant

--- scripts/gameLoader.js
+++ scripts/gameLoader.js
@@ -7,11 +7,14 @@ async function setGames() {
         game.id = "game" + i
         game.className = "game"
 
-        if (json.games[i].status === "live") {
-            game.innerHTML = "<section> <img src="+ json.games[i].home.logo +"> <p>" + json.games[i].home.score + "-" + json.games[i].away.score + "</p> <img src="+ json.games[i].away.logo +"> </section>"
+        if (json.games[i].status === "Live") {
+            game.innerHTML = "<section><div><p>" + json.games[i].status + "</p><div></div></div><section> <img src="+ json.games[i].home.logo +"> <p>" + json.games[i].home.score + "</p><p>-</p><p>" + json.games[i].away.score + "</p> <img src="+ json.games[i].away.logo +"> </section><p>" + json.games[i].arena + "</p> </section> <section> <a>Watch</a> <a> Buy Tickets</a> </section>"
             game.className = "game-live"
+            
         }
         
+
+
         document.getElementById("sliderGames").appendChild(game)
     }
 }
